7ac87c4a2908063dab6c88958964e1397fccbd43
jseungj <jseungj@jseungj-pc.mshome.net>
2019-09-01 13:47:23 -0400

    Changed variables

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -3,21 +3,23 @@ package a1;
 import java.util.Scanner;
 
 public class A1Novice {
-	public static int customers, itemsBought, itemAmount;
-    public static double price, finalPrice;
-    public static String first, last;
-    public static char firstInitial;
+	
     public static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-
+    	
+    	int customers, itemsBought, itemAmount;
+        double price, finalPrice = 0;
+        String firstName, lastName;
+        char firstInitial;
         customers = scan.nextInt();
+        
         for (int i = 0; i < customers; i++) {
-            first = scan.next();
-            last = scan.next();
-            firstInitial = first.charAt(0);
+            firstName = scan.next();
+            lastName = scan.next();
+            firstInitial = firstName.charAt(0);
             itemsBought = scan.nextInt();
-
+            
             for (int j = 0; j < itemsBought; j++) {
                 itemAmount = scan.nextInt();
                 scan.next();
@@ -25,7 +27,7 @@ public class A1Novice {
                 price = itemAmount * price;
                 finalPrice += price;
             }
-            System.out.println(firstInitial + ". " + last + ": " + String.format("%.2f", finalPrice));
+            System.out.println(firstInitial + ". " + lastName + ": " + String.format("%.2f", finalPrice));
             finalPrice = 0;
         }
     }

32e872ad697eda1910f5d21787511d6cecf800dd
jseungj <jseungj@jseungj-pc.mshome.net>
2019-09-01 12:59:10 -0400

    Finished A1Novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -3,12 +3,30 @@ package a1;
 import java.util.Scanner;
 
 public class A1Novice {
+	public static int customers, itemsBought, itemAmount;
+    public static double price, finalPrice;
+    public static String first, last;
+    public static char firstInitial;
+    public static Scanner scan = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		
-		Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
 
-		// Your code follows here.
-		
-	}
+        customers = scan.nextInt();
+        for (int i = 0; i < customers; i++) {
+            first = scan.next();
+            last = scan.next();
+            firstInitial = first.charAt(0);
+            itemsBought = scan.nextInt();
+
+            for (int j = 0; j < itemsBought; j++) {
+                itemAmount = scan.nextInt();
+                scan.next();
+                price = scan.nextDouble();
+                price = itemAmount * price;
+                finalPrice += price;
+            }
+            System.out.println(firstInitial + ". " + last + ": " + String.format("%.2f", finalPrice));
+            finalPrice = 0;
+        }
+    }
 }

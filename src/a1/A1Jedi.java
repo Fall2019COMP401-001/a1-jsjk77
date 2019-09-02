d2be1f1a9fe821b1476a3158e5f6ed2bedf80a8f
jseungj <jseungj@jseungj-pc.mshome.net>
2019-09-02 00:05:41 -0400

    Touched up A1 Jedi

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -1,3 +1,8 @@
+// Hey Jon...lmk if u see this
+// I can see this
+// nice :) 
+
+
 package a1;
 
 import java.util.Scanner;
@@ -10,14 +15,15 @@ public class A1Jedi {
 		int totalItemsCount, customers, totalUniqueItems, tempNumItems;
 		String[] itemName, firstName, lastName;
 		double[] itemPrices;
-		int[] itemCount, customerBuy;
+		int[] itemCount, customerCount;
+		boolean[] customerBuy;
 		String itemBought;
 		
 		totalItemsCount = scan.nextInt();
 		itemName = new String[totalItemsCount];
 		itemPrices = new double[totalItemsCount];
 		itemCount = new int[totalItemsCount];
-		customerBuy = new int[totalItemsCount];
+		customerCount = new int[totalItemsCount];
 		
 		for (int i = 0; i < totalItemsCount; i++) {
 			itemName[i] = scan.next();
@@ -32,25 +38,32 @@ public class A1Jedi {
 			firstName[i] = scan.next();
 			lastName[i] = scan.next();
 			totalUniqueItems = scan.nextInt();
+			customerBuy = new boolean[totalItemsCount];
 			
 			for (int j = 0; j < totalUniqueItems; j++) { 
 			    tempNumItems = scan.nextInt();
 			    itemBought = scan.next();
 			    
-				for (int k = 0; k < itemName.length; k++) {
+			for (int k = 0; k < itemName.length; k++) {
 					if (itemName[k].contains(itemBought)) {
 						itemCount[k] = itemCount[k] + tempNumItems;
-						customerBuy[k] = customerBuy[k] + 1;
+						customerBuy[k] = true;
 						}
+			}
+
+		}
+      			for (int l = 0; l < itemName.length; l++) {
+				  if (customerBuy[l]) {
+						customerCount[l] = customerCount[l] + 1;
 					}
 			}
 		}
 		for (int i = 0; i < itemCount.length; i++) {
-			if (customerBuy[i] == 0) {
+			if (customerCount[i] == 0) {
 				System.out.println("No customers bought " + itemName[i]);
 			}
 			else {
-			System.out.println(customerBuy[i] + " customers bought "+ itemCount[i] + " " + itemName[i]);
+			  System.out.println(customerCount[i] + " customers bought "+ itemCount[i] + " " + itemName[i]);
 			}
 		}
 		
@@ -60,4 +73,5 @@ public class A1Jedi {
 	
 
 
-	
+
+//I want to die.
